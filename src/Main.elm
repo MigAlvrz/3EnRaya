23d890ac31cec9ecef0839e39ab280fc21aac108
Miga <milual1988@gmail.com>
2023-10-23 10:54:39 +0200

    implementing basic game logic

--- src/Main.elm
+++ src/Main.elm
@@ -1,73 +1,135 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-import Dict exposing (update)
-import Html exposing (mark)
 import Dict exposing (Dict)
-import Element exposing (Element)
+import Element as E
 import Element.Border as Border
+import Element.Events exposing (onClick)
+import Element.Input as Input
+import Html exposing (Html, button, div, text)
 
-type alias Model = { tablero : Tablero }
-type alias Tablero = Dict (Int, Int) Mark
 
-type Mark = X | O
+
+type alias Model =
+    { tablero : Tablero, turno : Mark, ganador : Maybe Mark }
+
+
+type alias Tablero =
+    Dict ( Int, Int ) Mark
+
+
+type Mark
+    = X
+    | O
+
 
 initialModel : Model
-initialModel = 
-            { tablero = Dict.empty 
-                |> Dict.insert (1,1) X 
-                |> Dict.insert (0,2) O 
-                |> Dict.insert (1,3) X }
+initialModel =
+    { tablero = Dict.empty, turno = X, ganador = Nothing }
 
-type Msg = NoOp
+
+type Msg
+    = Marcar Int Int Mark
+    | Reset
+
+
+siguienteMarca : Mark -> Mark
+siguienteMarca mark =
+    case mark of
+        X ->
+            O
+
+        O ->
+            X
+
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        Marcar row col mark ->
+            let
+                actualizarTablero =
+                    Dict.insert ( row, col ) mark model.tablero
+            in
+            if Dict.member ( row, col ) model.tablero then
+                model
 
-viewGrid : Tablero -> Element Msg
-viewGrid tablero =
-    let
-        cellContent coords = 
-            Dict.get coords tablero
-            |> Maybe.map (\mark ->
-                case mark of X -> "X"
-                            O -> "O"
-                            )
-            |> Maybe.withDefault ""
+            else
+                { model
+                    | tablero = actualizarTablero
+                    , turno = siguienteMarca model.turno
+                }
+        Reset ->
+            initialModel
 
-        cell (row,column)  = Element.el [
-            Element.width <| Element.px 50,
-            Element.height <| Element.px 50,
-            Border.width 1
-            ] 
-            <| 
-               Element.el [Element.centerX, Element.centerY] (Element.text content)    
-    in
 
-    Element.column [Element.spacing 5] [
-        Element.row [Element.spacing 5] [cell (0,0), cell (0,1), cell (0,2)]
-        , Element.row [Element.spacing 5] [cell (1,0), cell (1,1), cell (1,2)]
-        , Element.row [Element.spacing 5] [cell (2,0), cell (2,1), cell (2,2)]
-    ]
+
+
 
 view : Model -> Html Msg
 view model =
-    Element.layout [] 
-    (Element.column [Element.centerX, Element.centerY] 
-        [ Element.el [Element.centerX, Element.centerY ] <| 
-            Element.text "tres en raya"
-            , viewGrid model.tablero
+    let
+
+        tablero =
+            E.column [ E.spacing 5, E.centerX, E.centerY ]
+                [ cellRow model 0
+                , cellRow model 1
+                , cellRow model 2
+                ]
+
+        info =
+            E.row [ E.spaceEvenly ]
+                [ E.text "turno de "
+                , ponerMarca model.turno
+                , Input.button [] { onPress = Just Reset, label = E.text " Reset" }
+                ]
+    in
+    E.layout [] <|
+        E.column [ E.spacing 15, E.centerX, E.centerY ]
+            [ tablero, info ]
+
+
+cellRow : Model -> Int -> E.Element Msg
+cellRow model row =
+    E.row [ E.spacing 5 ]
+        [ cell model row 0
+        , cell model row 1
+        , cell model row 2
         ]
-    )
+
+
+ponerMarca : Mark -> E.Element Msg
+ponerMarca mark =
+    case mark of
+        X ->
+            E.text "X"
+
+        o ->
+            E.text "O"
+
+
+cell : Model -> Int -> Int -> E.Element Msg
+cell model row col =
+    let
+        valor =
+            Dict.get ( row, col ) model.tablero
+                |> Maybe.map (\mark -> ponerMarca mark)
+                |> Maybe.withDefault E.none
+    in
+    E.el
+        [ E.width <| E.px 100
+        , E.height <| E.px 100
+        , Border.solid
+        , Border.width 1
+        , onClick <| Marcar row col model.turno
+        ]
+        (E.el [ E.centerX, E.centerY ] valor)
+
 
 main : Program () Model Msg
-main = 
+main =
     Browser.sandbox
-    {
-        init = initialModel
+        { init = initialModel
         , view = view
         , update = update
-    }
+        }
